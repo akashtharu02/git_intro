d3c1435f3aeef5db1d9b8e7152478fac299ec422
Akash Tharu <akastharu270@gmail.com>
2025-11-04 08:08:22 +0545

    my first commit

---
+++ src/main/java/test/sum.java
@@ -0,0 +1,14 @@
+package test;
+
+import static sum.sum1;
+
+public class sum {
+    static int a = 5;
+    static int b = 3;
+    static int sum1 =a + b;
+
+}
+
+public static void main(String[] args) {
+    System.out.println("The sum of two numbers is : " +sum1);
+}
